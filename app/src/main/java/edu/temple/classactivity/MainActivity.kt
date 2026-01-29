b0581e03ed01ba0c9d65f43f2639479153ebf3e8
Elio Berberi <tuo92727@temple.edu>
2026-01-29 12:16:14 -0500

    failed linear stuff

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,6 +1,11 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.CompoundButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +15,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.mainText)
+        val increaseButton = findViewById<Button>(R.id.incButton)
+        val decreaseButton = findViewById<Button>(R.id.decButton)
+        val checkBox = findViewById<CheckBox>(R.id.checkBox)
+
+        var textSize = 17f
+        textView.textSize = textSize
+
+        increaseButton.isEnabled = false
+        decreaseButton.isEnabled = false
+
+        checkBox.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener {
+            override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
+                increaseButton.isEnabled = isChecked
+                decreaseButton.isEnabled = isChecked
+            }
+        })
+
+        increaseButton.setOnClickListener {
+            textSize += 4f
+            textView.textSize = textSize
+        }
+
+        decreaseButton.setOnClickListener {
+            textSize -= 2f
+            textView.textSize = textSize
+        }
     }
 }
